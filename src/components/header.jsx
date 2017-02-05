ae5804aa34fe1decd16d42ed62d6b9e2122a42ec
mthomps4 <matthew.thompson.a@gmail.com>
2017-02-05 16:47:14 -0500

    Stopwatch Start/Stop/Reset

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Player from './player.jsx';
 import Stats from './stats.jsx';
+import Stopwatch from './stopWatch.jsx';
 
 
 const Header = (props) =>
     <div className="header">
       <Stats players={props.players} />
       <h1>{props.title}</h1>
+      <Stopwatch />
     </div>
 ;
 
